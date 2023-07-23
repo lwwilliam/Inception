f31a3c0fdfb7c0dfb53d83218f674dfaad3013e5
lwwilliam <lee1052william@gmail.com>
2023-07-24 00:06:27 +0800

    wordpress looks weird

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -23,15 +23,12 @@ sed -i -r "s/MYSQL_LOCALHOST/$MYSQL_LOCALHOST/1"    wp-config.php
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-wp theme install astra --activate --allow-root
-wp plugin install redis-cache --activate --allow-root
+wp theme install twentytwentytwo --activate --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir /run/php
 
-wp redis enable --allow-root
-
 /usr/sbin/php-fpm7.4 -F
 
-tail -f /dev/null
+# tail -f /dev/null
